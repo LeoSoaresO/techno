d5170e000f792aa1b67d008ebf51d50cd797c8a0
LeoSoaresO <leonardo.rock22@hotmail.com>
2024-02-22 21:18:17 -0300

    techno project with fake api

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,81 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import './style.scss'
+export default {
+  data() {
+    const data:any[] = []
+    const product:any = {}
+    return {data, product}
+  },
+
+  methods: {
+    getAllProducts() {     
+      fetch("/src/api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.data = r
+          console.log(this.data);
+      })
+    },
+
+    getProductById(id:any) {     
+      fetch(`/src/api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.product = r
+          console.log(this.product);
+      })
+    },
+
+    closeModal(event:Event){
+      if(event.target === event.currentTarget) this.product = false;
+    },
+
+    price(value:any){
+      return value.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
+    } 
+  },
+
+  created() {
+    this.getAllProducts()
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <section class="modal" @click="closeModal" v-if="product.nome">
+        <div class="m_cont">
+          <div class="m_img">
+            <img :src="product.img" :alt="product.nome">
+          </div>
+          <div class="m_data">
+            <button @click="product.nome = false" class="m_btn_close">X</button>
+            <span class="m_price">{{ price(product.preco) }}</span>
+            <h2 class="m_title">{{ product.nome }}</h2>
+            <p>{{ product.descricao }}</p>
+            <button class="m_btn">Adiocionar Item</button>
+          </div>
+          <div class="rating">
+            <h2 class="rating_label">Avaliações</h2>
+            <ul >
+              <li class="review" v-for="p in product.reviews" :key="p.id">
+                <p class="r_descrption">{{ p.descricao }}</p>
+                <p class="r_user">{{ p.nome }} | {{ p.estrelas }} estrelas</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+      <section class="products">
+        <div v-for="d in data" @click="getProductById(d.id)" class="product" :key="d.id">
+          <img :src="d.img" :alt="d.nome" class="product_img">
+          <div class="p_info">
+            <span class="p_price">{{ price(d.preco) }}</span>
+            <h2 class="p_name">{{ d.nome }}</h2>
+          </div>
+        </div>
+      </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
