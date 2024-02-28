c8366368e642b1655584291772a4dbfa39f57a1d
LeoSoaresO <leonardo.rock22@hotmail.com>
2024-02-28 02:07:21 -0300

    Alert de produto adicionado ao carrinho

--- src/App.vue
+++ src/App.vue
@@ -8,54 +8,67 @@ export default {
     components: {HeaderComponent},
 
     data() {
-        const data:any[] = []
-        const cart:any[] = []
-        const product:any = {}
-        return {data, product, cart}
+        const data:any[] = [];
+        const cart:any[] = [];
+        const product:any = {};
+        const alertActive: boolean = false;
+        const alertText: string = '';
+        return {data, product, cart, alertActive, alertText}
     },
 
     methods: {
         getAllProducts() {     
-        fetch("/src/api/produtos.json")
-            .then(r => r.json())
-            .then(r => {
-            this.data = r
-            console.log(this.data);
-        })
+            fetch("/src/api/produtos.json")
+                .then(r => r.json())
+                .then(r => {
+                this.data = r
+                console.log(this.data);
+            })
         },
 
         getProductById(id:any) {     
-        fetch(`/src/api/produtos/${id}/dados.json`)
-            .then(r => r.json())
-            .then(r => {
-            this.product = r
-        })
+            fetch(`/src/api/produtos/${id}/dados.json`)
+                .then(r => r.json())
+                .then(r => {
+                this.product = r
+            })
         },
 
         openModal(id:any){
-        this.getProductById(id)
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
+            this.getProductById(id)
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
         },
 
         closeModal(event:Event){
-        if(event.target === event.currentTarget) this.product = false;
+            if(event.target === event.currentTarget) this.product = false;
         },
 
         price(value:any){
-        return value.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
+            return value.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
         },
 
         addItem(){
             this.product.estoque--;
             const{ id, nome, preco} = this.product
             this.cart.push({ id, nome, preco})
+            this.handleAlert(`${nome} adiocionado ao carrinho!`)
         },
+
         handleItemRemovido(data:any) {
             this.cart = data
-        }
+        },
+        
+        handleAlert(msg:string){
+            console.log(this.alertActive);
+            this.alertText = msg;
+            this.alertActive = true
+            setTimeout(() => {
+            this.alertActive = false
+            }, 2000);
+        }   
     },
     created() {
         this.getAllProducts();
@@ -68,39 +81,44 @@ export default {
 
     <HeaderComponent :cart="cart" @item-removido="handleItemRemovido"></HeaderComponent>
 
-  <section class="products">
-    <div v-for="d in data" @click="openModal(d.id)" class="product" :key="d.id">
-      <img :src="d.img" :alt="d.nome" class="product_img">
-      <div class="p_info">
-        <span class="p_price">{{ price(d.preco) }}</span>
-        <h2 class="p_name">{{ d.nome }}</h2>
-      </div>
-    </div>
-  </section>
+    <section class="products">
+        <div v-for="d in data" @click="openModal(d.id)" class="product" :key="d.id">
+            <img :src="d.img" :alt="d.nome" class="product_img">
+            <div class="p_info">
+                <span class="p_price">{{ price(d.preco) }}</span>
+                <h2 class="p_name">{{ d.nome }}</h2>
+            </div>
+        </div>
+    </section>
 
-  <section class="modal" @click="closeModal" v-if="product.nome">
-    <div class="m_cont">
-      <div class="m_img">
-        <img :src="product.img" :alt="product.nome">
-      </div>
-      <div class="m_data">
-        <button @click="product.nome = false" class="m_btn_close">X</button>
-        <span class="m_price">{{ price(product.preco) }}</span>
-        <h2 class="m_title">{{ product.nome }}</h2>
-        <p>{{ product.descricao }}</p>
-        <button v-if="product.estoque > 0" class="m_btn" @click="addItem()">Adiocionar Item</button>
-        <button v-else class="m_btn out_stock" disabled>Sem Etoque</button>
-      </div>
-      <div class="rating">
-        <h2 class="rating_label">Avaliações</h2>
-        <ul>
-          <li class="review" v-for="p in product.reviews" :key="p.id">
-            <p class="r_descrption">{{ p.descricao }}</p>
-            <p class="r_user">{{ p.nome }} | {{ p.estrelas }} estrelas</p>
-          </li>
-        </ul>
-      </div>
+    <section class="modal" @click="closeModal" v-if="product.nome">
+        <div class="m_cont">
+            <div class="m_img">
+                <img :src="product.img" :alt="product.nome">
+            </div>
+            <div class="m_data">
+                <button @click="product.nome = false" class="m_btn_close">X</button>
+                <span class="m_price">{{ price(product.preco) }}</span>
+                <h2 class="m_title">{{ product.nome }}</h2>
+                <p>{{ product.descricao }}</p>
+                <button v-if="product.estoque > 0" class="m_btn" @click="addItem()">Adiocionar Item</button>
+                <button v-else class="m_btn out_stock" disabled>Sem Etoque</button>
+            </div>
+            <div class="rating">
+                <h2 class="rating_label">Avaliações</h2>
+                <ul>
+                    <li class="review" v-for="p in product.reviews" :key="p.id">
+                        <p class="r_descrption">{{ p.descricao }}</p>
+                        <p class="r_user">{{ p.nome }} | {{ p.estrelas }} estrelas</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <div class="alert" :class="{active: alertActive}">
+        <p class="alert_text">{{alertText}}</p>
     </div>
-  </section>
+
 </template>
 
