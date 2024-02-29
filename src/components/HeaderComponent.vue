e200be967ca8176ce047df7f8eb179f6ef101377
LeoSoaresO <leonardo.rock22@hotmail.com>
2024-02-28 23:12:59 -0300

    Estilo carrinho

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,7 +5,8 @@ import './header.scss';
 export default {
     data() {
         const cartControl:any = []
-        return {cartControl}
+        const cartActive: boolean = false;
+        return {cartControl, cartActive}
     },
     props: {
         cart: {
@@ -34,7 +35,7 @@ export default {
         }
     },
     methods: {
-        removeItem(index:any, id:any) {
+        removeItem(index:any) {
             this.cartControl.splice(index, 1)
             this.$emit('item-removido', this.cartControl);
         },
@@ -45,7 +46,10 @@ export default {
             if(window.localStorage.cart.length > 0){
                 this.cartControl = JSON.parse(window.localStorage.cart)
             }
-        }
+        },
+        closeModal(event:Event){
+            if(event.target === event.currentTarget) this.cartActive = false;
+        },
     },
     created() {
         this.cartControl = this.cart;
@@ -58,12 +62,26 @@ export default {
 <template>
   <header id="header-component" class="header">
     <img src="/src/assets/techno.svg" class="logo" alt="">
-    <div class="cart">{{ price(cartTotal) }} | {{ cartControl.length }}</div>
-    <ul v-if="cartControl">
-        <li v-for="(i, index) in cartControl" :key="i.id">
-            <p>{{ i.nome }}</p>
-            <button @click="removeItem(index, i.id)">X</button>
-        </li>
-    </ul>
+    <div class="cart" @click="cartActive = true">{{ price(cartTotal) }} | {{ cartControl.length }}</div>
   </header>
+  
+  <section class="cart_modal" :class="{active: cartActive}" @click="closeModal">
+    <div class="cart_cont">
+        <button class="cart_btn_close" @click="cartActive = false">X</button>
+        <h2 class="cart_title">Carrinho</h2>
+        <div v-if="cartControl.length > 0">
+            <ul class="cart_list">
+                <li class="cart_item" v-for="(i, index) in cartControl" :key="i.id">
+                    <p class="cart_item_name">{{ i.nome }}</p>
+                    <p class="cart_item_price">{{ price(i.preco) }}</p>
+                    <button class="cart_item_remove" @click="removeItem(index, i.id)">X</button>
+                </li>
+            </ul>
+            <p class="cart_total">{{ price(cartTotal) }}</p>
+            <button class="cart_close">Finalizar Compra</button>
+        </div>
+        <p v-else>Carrinho vazio</p>
+    </div>
+  </section>
+
 </template>
